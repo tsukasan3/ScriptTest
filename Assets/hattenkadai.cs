c17d1f7287ee42f24a158b12a36eeaa3b24175cb
大原聖司 <aterm.0513@gmail.com>
2017-08-25 19:49:13 +0900

    ScriptTest 課題
    課題と発展課題の提出

---
+++ Assets/hattenkadai.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss{
+	private int mp = 53;	//MP
+	
+	public void Magic(){
+		if(mp >= 5){
+			this.mp -= 5;
+			Debug.Log("魔法攻撃をした。残りMPは" + this.mp +"。");
+		}else{
+			Debug.Log("MPが足りないため魔法が使えない。");
+		}
+	}
+	
+}
+
+public class hattenkadai : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		Boss lastboss = new Boss();
+		
+		for(int i = 1; i < 12; i++){
+			//魔法攻撃用の関数を呼び出す
+			lastboss.Magic();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/kadai.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kadai: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//要素数5の配列を初期化する
+		int[] array = {1, 2, 3, 4, 5};
+		
+		//配列の各要素を順番に表示する
+		for(int i = 0; i < 5; i++){
+			Debug.Log(array[i]);
+		}
+		
+		//配列の各要素を逆順に表示する
+		for(int i = 5; i > 0 ; i--){
+			Debug.Log(array[i-1]);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
